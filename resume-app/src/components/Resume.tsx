1d0ca279e444b0e04630385c54c97f79c233e05d
z89 <z89>
2021-02-20 01:00:37 +1000

    changed homepage in package.json

--- resume-app/src/components/Resume.tsx
+++ resume-app/src/components/Resume.tsx
@@ -1,35 +1,26 @@
 import { Row, Col } from "react-bootstrap";
 import RellaxWrapper from "react-rellax-wrapper";
+import { Document, Page } from "react-pdf";
+import { useState } from "react";
 
 const Resume = () => {
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
   return (
     <>
       <div className="home">
         <section id="first">
           <Row className="align-items-center">
             <Col>
-              <RellaxWrapper percentage={0.5} speed={-7}>
-                <h1>I am really fast!</h1>
-              </RellaxWrapper>
-              <RellaxWrapper percentage={0.5} speed={-4}>
+              <RellaxWrapper percentage={0.5} speed={-8}>
                 <div>
-                  <p>I am really slow</p>
-                </div>
-              </RellaxWrapper>
-            </Col>
-          </Row>
-        </section>
-      </div>
-      <div className="info">
-        <section id="second">
-          <Row className="align-items-center">
-            <Col>
-              <RellaxWrapper percentage={0.2} speed={5}>
-                <h1>I am really fast!</h1>
-              </RellaxWrapper>
-              <RellaxWrapper speed={2}>
-                <div>
-                  <p>I am really slow</p>
+                  <Document file="resume.pdf">
+                    <Page pageNumber={pageNumber} />
+                  </Document>
+                  <p>
+                    Page {pageNumber} of {numPages}
+                  </p>
                 </div>
               </RellaxWrapper>
             </Col>
