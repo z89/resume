5b556af9e7d3a3d88c3249e8e93d9ca68720ab8c
Zeo <30657227+z89@users.noreply.github.com>
2021-03-08 03:26:54 +1000

    added next.js app

---
+++ app/pages/index.tsx
@@ -0,0 +1,13 @@
+import Navigation from "../components/Navigation";
+import Portfolio from "../components/Portfolio";
+
+export default function Home() {
+  return (
+    <>
+      <div className="App">
+        <Navigation />
+        <Portfolio />
+      </div>
+    </>
+  );
+}
