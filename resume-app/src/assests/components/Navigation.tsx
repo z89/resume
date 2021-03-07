4206bac9664cbdbf3da009f391e50939f348e2cb
Zeo <30657227+z89@users.noreply.github.com>
2021-03-07 23:17:22 +1000

    changed movement function slightly :smile

--- resume-app/src/assests/components/Navigation.tsx
+++ resume-app/src/assests/components/Navigation.tsx
@@ -18,6 +18,7 @@ const Navigation = () => {
   const handleOnClick = (icon: string) => {
     switch (icon) {
       case "Move":
+        console.log("move toggle!");
         setMove(!move);
         break;
       case "Droplet":
@@ -28,67 +29,89 @@ const Navigation = () => {
     }
   };
 
-  const navItemsArr = [
-    { iconName: "Book", icon: () => <Book /> },
+  const itemsArr = [
+    {
+      iconName: "Book",
+      icon: () => <Book onMouseDown={() => console.log("I'm a book!")} />,
+    },
     { iconName: "Activity", icon: () => <Activity /> },
     { iconName: "Droplet", icon: () => <Droplet /> },
-    { iconName: "Move", icon: () => <Move /> },
-    { iconName: "RefreshCw", icon: () => <RefreshCw /> },
+    {
+      iconName: "Move",
+      icon: ({
+        onMouseDown,
+        onMouseUp,
+
+        className,
+      }: {
+        onMouseDown: () => void;
+        onMouseUp: () => void;
+
+        className?: string;
+      }) => (
+        <Move
+          onMouseDown={onMouseDown}
+          onMouseUp={onMouseUp}
+          className={className}
+          style={{ width: "auto" }}
+        />
+      ),
+    },
+    {
+      iconName: "RefreshCw",
+      icon: ({ className }: { className?: string }) => (
+        <RefreshCw className={className} />
+      ),
+    },
     { iconName: "GitHub", icon: () => <GitHub /> },
     { iconName: "Mail", icon: () => <Mail /> },
     { iconName: "Key", icon: () => <Key /> },
   ];
 
-  const navItems = Object.values(navItemsArr).map((item, index) => {
+  const navItems = Object.values(itemsArr).map((item, index) => {
     const Icon = item.icon;
     const iconName = item.iconName;
 
     return (
-      <li className="nav-item">
-        <div
-          onClick={() => {
-            handleOnClick(iconName);
-          }}
-          className={move ? "active nav-link" : "nav-link"}
-        >
-          <Icon key={index} />
+      <li key={index} className="nav-item">
+        <div className={move ? "active nav-link" : "nav-link"}>
+          <Icon
+            onMouseDown={() => {
+              console.log("down");
+              handleOnClick(iconName);
+            }}
+            onMouseUp={() => console.log("up")}
+          />
         </div>
       </li>
     );
   });
 
+  function jsx() {
+    return (
+      <nav className="navbar">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a href="/#" className="brand-link">
+              <Brand className="brand" />
+            </a>
+          </li>
+          {navItems}
+        </ul>
+      </nav>
+    );
+  }
+
   if (move) {
     return (
       <>
-        <Draggable defaultClassNameDragging="dragging">
-          <nav className="navbar">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a href="/#" className="brand-link">
-                  <Brand className="brand" />
-                </a>
-              </li>
-              {navItems}
-            </ul>
-          </nav>
+        <Draggable allowAnyClick={true} defaultClassNameDragging="dragging">
+          {jsx()}
         </Draggable>
       </>
     );
   } else {
-    return (
-      <>
-        <nav className="navbar">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a href="/#" className="brand-link">
-                <Brand className="brand" />
-              </a>
-            </li>
-            {navItems}
-          </ul>
-        </nav>
-      </>
-    );
+    return <>{jsx()}</>;
   }
 };
 
