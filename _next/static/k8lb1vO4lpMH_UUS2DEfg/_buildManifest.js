9627fe62bd1dd672bac910e383f7e87f1052bcff
z89 <z89@users.noreply.github.com>
2021-03-08 08:02:41 +0000

    deploy: ce39f05fc75ad880597b2698523f4c97a57c2098

---
+++ _next/static/k8lb1vO4lpMH_UUS2DEfg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-fdd901fbce9969708c60.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b638bf88f61a9acd041d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
